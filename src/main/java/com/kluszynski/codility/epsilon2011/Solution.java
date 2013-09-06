63d0a42bdc81c0f206ec2cf59eee0cb5f29be28f
Krzysztof Luszynski <dyermaker@poczta.onet.pl>
2013-09-06 13:03:49 +0200

    Corrected imports

--- src/main/java/com/kluszynski/codility/epsilon2011/Solution.java
+++ src/main/java/com/kluszynski/codility/epsilon2011/Solution.java
@@ -2,9 +2,7 @@ package com.kluszynski.codility.epsilon2011;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * The Class Solution.
